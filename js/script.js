c7be466360d8b04ce8407852e54f3ae36b4a45c8
Soumya Ranjan Naik <srnaik2209@gmail.com>
2020-02-03 21:00:03 +0530

    .

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  // Transition effect for navbar 
+  $(window).scroll(function () {
+    // checks if window is scrolled more than 500px, adds/removes solid class
+    if ($(this).scrollTop() > 500) {
+      $('.navbar').addClass('solid');
+    } else {
+      $('.navbar').removeClass('solid');
+    }
+  });
+});
+
+$(".hover").mouseleave(
+  function () {
+    $(this).removeClass("hover");
+  }
+);
